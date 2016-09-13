804913bcb0d2d96279d710c086b075e9680d7eaf
GIRAFF <k.yngvarr@gmail.com>
2016-09-13 18:18:46 +0700

    Lab1: works for Trakimus, but hell for us.

--- lab1/src/main.cc
+++ lab1/src/main.cc
@@ -1,4 +1,5 @@
 #include "main.h"
+#define DEBUG
 
 typedef enum
 {
@@ -6,7 +7,7 @@ typedef enum
 	PROFILE
 } mtx_fmt_e;
 
-typedef real_t float;
+typedef float real_t;
 
 typedef struct
 {
@@ -14,13 +15,13 @@ typedef struct
 	mtx_fmt_e fmt;
 	union
 	{
-		real_t *tri = nullptr;
+		real_t *tri;
 		struct
 		{
-			real_t *di = nullptr,
-				*ia = nullptr,
-				*al = nullptr,
-				*au = nullptr;
+			real_t *di,
+				*al,
+				*au;
+			size_t *ia;
 		};
 	};
 	real_t elem(size_t row, size_t col);
@@ -30,22 +31,51 @@ typedef struct
 {
 	size_t n;
 	real_t *v;
+	real_t& operator[](const int idx);
 } vector_t;
 
+// TODO do the same with [][] or (,)
 real_t matrix_t::elem(size_t row, size_t col)
 {
 	switch(fmt)
 	{
 		case TRIANGLE:
+		{
 			if(col > row) return 0;
 			size_t num = row;
 			for(size_t i = 1; i < row; ++i) num += i;
 			num += col;
 			return tri[num];
-		break;
+			break;
+		}
 		case PROFILE:
-			// TODO get what we need
-		break;
+		{
+			if(row == col) return di[row];
+			if(row > col)
+			{
+				size_t len = ia[row+1] - ia[row];
+				return col < row-len ? 0 :
+					al[ia[row]+col-(row-len)];
+			}
+			size_t len = ia[col+1] - ia[col];
+			return row < col-len ? 0 : au[ia[col]+row-(col-len)];
+			break;
+		}
+	}
+}
+
+real_t& vector_t::operator[](const int idx)
+{
+	return v[idx];
+}
+
+void print_mtx(matrix_t m, std::ostream& out)
+{
+	for(size_t i = 0; i < m.n; ++i)
+	{
+		for(size_t j = 0; j < m.n; ++j)
+			out << m.elem(i,j) << ' ';
+		out << '\n';
 	}
 }
 
@@ -64,11 +94,12 @@ int main(int argc, char **argv)
 	matrix_t A;
 	vector_t F;
 	vector_t x;
+	vector_t y;
 	// A = LL^T
 	matrix_t L;
-	// because keepin' L in profile form is not a good idea
-	A.fmt = mtx_fmt_e.PROFILE;
-	L.fmt = mtx_fmt_e.TRIANGLE;
+	// because storin' L in profile form is not a good idea
+	A.fmt = PROFILE;
+	L.fmt = TRIANGLE;
 	// get N
 	std::cin >> A.n;
 	// allocate memory for L
@@ -78,11 +109,13 @@ int main(int argc, char **argv)
 	for(size_t i = 1; i < A.n; ++i) L.n += i;
 	L.tri = new real_t[L.n];
 	// copy n for organizational purposes
-	F.n = x.n = L.n = A.n;
+	F.n = x.n = y.n = L.n = A.n;
 	// allocate some memory
 	F.v = new real_t[F.n];
+	x.v = new real_t[x.n];
+	y.v = new real_t[y.n];
 	A.di = new real_t[A.n];
-	A.ia = new real_t[A.n+1];
+	A.ia = new size_t[A.n+1];
 	// read profile
 	for(size_t i = 0; i < A.n+1; ++i)
 		std::cin >> A.ia[i];
@@ -99,9 +132,63 @@ int main(int argc, char **argv)
 	for(size_t i = 0; i < F.n; ++i)
 		std::cin >> F.v[i];
 
+#ifdef DEBUG
+	std::cerr << "Here comes A:\n";
+	print_mtx(A, std::cerr);
+	std::cerr << '\n';
+#endif
+
 	// TODO now, let's find L
+	size_t idx = 0;
+	for(size_t i = 0; i < A.n; ++i)
+	for(size_t j = 0; j <= i; ++j)
+	{
+		real_t sum = 0;
+		if(i == j)
+		{
+			for(size_t k = 0; k < i; ++k) sum += pow(L.elem(k,i),2);
+			L.tri[idx++] = sqrt(A.elem(i,i) - sum);
+			continue;
+		}
+		for(size_t k = 0; k < i; ++k) sum += L.elem(i,k)*L.elem(j,k);
+		L.tri[idx++] = (A.elem(i,j) - sum)/L.elem(j,j);
+	}
+	
+#ifdef DEBUG
+	std::cerr << "Here comes L:\n";
+	print_mtx(L, std::cerr);
+	std::cerr << '\n';
+#endif
+
 	// TODO find y
+	for(size_t i = 0; i < y.n; ++i)
+	{
+		real_t sum = 0;
+		for(size_t k = 0; k < i; ++k) sum += L.elem(i,k)*y[k];
+		y[i] = (F[i] - sum)/L.elem(i,i);
+	}
+	
+#ifdef DEBUG
+	std::cerr << "y:\n";
+	for(size_t i = 0; i < y.n; ++i) std::cerr << y[i] << ' ';
+	std::cerr << '\n';
+#endif
+
 	// TODO find x
+	for(int64_t i = x.n - 1; i >= 0; --i)
+	{
+		real_t sum = 0;
+		for(size_t k = i + 1; k < x.n; ++k) sum += L.elem(k,i)*x[k];
+		x[i] = (y[i] - sum)/L.elem(i,i);
+	}
+	
+#ifdef DEBUG
+	std::cerr << "x:\n";
+	for(size_t i = 0; i < x.n; ++i) std::cerr << x[i] << ' ';
+	std::cerr << "\n\n";
+#endif
+
+	for(size_t i = 0; i < x.n; ++i) std::cout << x[i] << '\n';
 
 	return 0;
 }
--- lab1/src/main.h
+++ lab1/src/main.h
@@ -1,7 +1,10 @@
 #ifndef _LAB1_MAIN
 #define _LAB1_MAIN
 
+#define _USE_MATH_DEFINES
+
 #include <iostream>
 #include <cstdint>
+#include <cmath>
 
 #endif //_LAB1_MAIN
