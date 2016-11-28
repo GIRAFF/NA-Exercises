53ff0acb6d9204e6fc9b33b2bf2ec26bc3f3ba6c
GIRAFF <k.yngvarr@gmail.com>
2016-11-28 21:57:33 +0700

    It works and generates report. By itself!

--- lab2/src/main.cc
+++ lab2/src/main.cc
@@ -20,7 +20,6 @@ public:
 		for (uint i = 0; i < N; ++i) free(di[i]);
 		free(di);
 		free(x);
-		//free(jx);
 		free(f);
 	}
 	/* Input file:
@@ -63,9 +62,6 @@ public:
 		for (uint i = 0; i < n; ++i)
 		for (uint k = 0; k < N; ++k) {
 			int j = Jee(m,k,i);
-//#ifdef DEBUG
-		//std::cerr << "j = " << j << std::endl;
-//#endif
 			ax[i] += j >= 0 && j < n ? di[k][i]*x[j] : 0;
 		}
 		for (uint i = 0; i < n; ++i) {
@@ -74,10 +70,6 @@ public:
 		real nax = norm(ax,n);
 		real nf = norm(f,n);
 		real res = nax / nf;
-//#ifdef DEBUG
-		//std::cerr << "Residual is " << nax << '/' << nf
-			//<< '=' << res << std::endl;
-//#endif
 		free(ax);
 		return res;
 	}
@@ -87,9 +79,6 @@ public:
 			iterate(method);
 			if (residual() < eps) break;
 		}
-//#ifdef DEBUG
-		//std::cerr << "Total number of iterations: " << iter << std::endl;
-//#endif
 	}
 	void iterate(int method)
 	{
@@ -103,10 +92,6 @@ public:
 			}
 			x[i] = jx[i] + (w/di[ZRODI][i])*(f[i] - sum);
 		}
-//#ifdef DEBUG
-		//std::cerr << "x: ";
-		//print_x(std::cerr);
-//#endif
 	}
 #undef Jee
 	void print_x(std::ostream& out)
@@ -118,17 +103,16 @@ public:
 	void find_w(int method, std::ostream& out)
 	{
 		real v[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-		real pit = 200001;
-		real step;
 		out << "\\begin{longtable}{|c|c|c|c|} \\hline\n";
 		out << "\\(\\omega\\)&\\(x\\)&\\(x^*-x\\)&Итераций\\\\ \\hline\n";
-		if (JACOBI == method) w = step = .5;
-		else w = step = 1;
-		while (step > .01) {
+		real max = JACOBI == method ? 1 : 2;
+		for (w = .05; w < max; w += .05) {
 			iter = 0;
 			memset(x, 0, n*sizeof(real));
 			solve(method);
+			out << std::setprecision(3);
 			out << "\\(" << w << "\\) &\n\\(\\begin{aligned} ";
+			out << std::setprecision(20);
 			for (int i = 0; i < n; ++i) {
 				out << "& " << x[i] << (i == n-1 ? " " : " \\\\ ");
 			}
@@ -138,20 +122,13 @@ public:
 			}
 			out << "\\end{aligned}\\) &\n\\("
 				<< iter << "\\) \\\\ \\hline\n";
-			step = step/2;
-			if (iter < pit) w += step;
-			else w -= step;
-			pit = iter;
 		}
 		out << "\\end{longtable}\n";
 	}
-	uint total_iter()
-	{
-		return iter;
-	}
+	uint n;
 	real *x;
 private:
-	uint n, m, max, iter;
+	uint m, max, iter;
 	real w, eps, **di, *f, *jx;
 };
 
@@ -160,8 +137,14 @@ int main(int argc, char **argv)
 	std::cout << std::setprecision(20);
 	SLE sle;
 	sle.init(std::cin);
-	//sle.solve(JACOBI);
+	sle.solve(GAUSS);
 	//sle.print_x(std::cout);
-	sle.find_w(JACOBI, std::cout);
+	//sle.find_w(JACOBI, std::cout);
+	real v[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+	for (uint i = 0; i < sle.n; ++i) {
+		v[i] -= sle.x[i];
+	}
+	std::cout << (sle.norm(v, sle.n)/sle.norm(sle.x, sle.n)) /
+		(sle.residual()) << std::endl;
 	return 0;
 }
